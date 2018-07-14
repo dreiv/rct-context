8657781dd3f2224298493d5e867002fe53e8ad9d
dreiv <dreivoicu@gmail.com>
2018-07-14 12:38:44 +0300

    added styling + deploy

--- src/components/DealerCard.js
+++ src/components/DealerCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import DealerProfile from './DealerProfile'
 import DealerActions from './DealerActions'
+import './DealerCard.css'
 
 export const DealerCard = () => (
 	<div className="dealer-page">
